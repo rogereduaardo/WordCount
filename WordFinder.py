02a45de40f744c1cf3e6a7c821ed3ac3e3a1bd2b
Roger Martinez <remmtools98@gmail.com>
2025-03-19 10:17:33 -0700

    fixed misspelled "word"

--- WordFinder.py
+++ WordFinder.py
@@ -1,17 +1,21 @@
-name = input('Enter file:')
-handle = open(name, 'r')
-counts = dict()
+name = input('Enter file:') #name variable
+handle = open(name, 'r') #opens the file on read mode
+counts = dict() #created an empty dictionary
 
+#Nested for loop
 for line in handle:
-    words = line.split()
-    for words in words:
+    words = line.split() #split a line of text into separate words
+    for word in words:
         counts[word] = counts.get(word, 0) + 1
 
+#find the most common word
 bigcount = None
 bigword = None
+
+#for loop
 for word, count in list(counts.items()):
     if bigcount is None or count > bigcount:
-        bigword = word
-        bigcount = count
+        bigword = word #returns the most common word
+        bigcount = count #returns the number of times the word repeats
  
-print(bigword, bigcount)       
+print(bigword, bigcount)
